c6c2e1849245cac5a64cd220b798beb492eaf084
Omershlomo1 <omershlomo441@gmail.com>
2022-08-26 17:23:50 +0300

    First commit

---
+++ djangochat/core/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+
+from. import views 
+
+
+urlpatterns = [
+    path('', views.frontpage, name='frontpage'),
+    path('signup/',views.signup, name='sign up')
+    
+]
