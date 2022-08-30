d5ed2bb7b649401b966e6822e6aa5472a5f3e985
Omer Shlomo <87198471+Omeritzio@users.noreply.github.com>
2022-08-30 23:20:29 +0300

    Revert "Log out"

--- djangochat/core/urls.py
+++ djangochat/core/urls.py
@@ -1,13 +1,10 @@
-from unicodedata import name
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
 
 from. import views 
 
 
 urlpatterns = [
     path('', views.frontpage, name='frontpage'),
-    path('signup/',views.signup, name='sign up'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout')
+    path('signup/',views.signup, name='sign up')
     
 ]
