5d46901963b5cc1dd5a4c35d5ffb320a2d54b1c7
Omershlomo1 <omershlomo441@gmail.com>
2022-08-31 18:13:29 +0300

    forms refresh

--- djangochat/core/forms.py
+++ djangochat/core/forms.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import User
 class SignUpForm(UserCreationForm):
     class Meta:
         model = User
-        fields ={'Username','password1','password2'}
+        fields ={'username','password1','password2'}
