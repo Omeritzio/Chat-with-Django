1b4f8eaf6675dbf059d0a69f3e13c159970edc4a
Omershlomo1 <omershlomo441@gmail.com>
2022-09-01 09:40:16 +0300

    forms refresh

--- djangochat/core/urls.py
+++ djangochat/core/urls.py
@@ -8,7 +8,7 @@ from. import views
 urlpatterns = [
     path('', views.frontpage, name='frontpage'),
     path('signup/',views.signup, name='sign up'),
-    path('login/' ,auth.view.LoginView.as_view(tem))
+    path('login/' ,auth_views.LoginView.as_view(template_name='core/logout.html'), name='login'),
     path('logout/',auth_views.LogoutView.as_view(), name='logout')
     
 ]
