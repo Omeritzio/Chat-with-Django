d6dc168903a204bd5655eed2e934b74d299d984a
Omershlomo1 <omershlomo441@gmail.com>
2022-09-01 22:46:12 +0300

    Messages

--- djangochat/room/consumers.py
+++ djangochat/room/consumers.py
@@ -7,7 +7,7 @@ from asgiref.sync import sync_to_async
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.room_name=self.scope['url_route']['kwargs']['room_name']
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name= 'chat_%s' % self.room_name
 
 
@@ -28,18 +28,29 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     async def recive(self, text_data):
         data = json.loads(text_data)
-        message =data['message']
-        username =data['username']
-        room =data['room']
+        message = data['message']
+        username = data['username']
+        room = data['room']
 
-        await self.channel.layer.group_send(
+        await self.channel_layer.group_send(
             self.room_group_name,
             {
-
-                'type':'chat_massege',
+                'type':'chat_messege',
                 'message':message,
                 'username':username,
                 'room':room,
             }
 
         )
+    async def chat_message(self, event):
+        message = event['message']
+        username = event['username']
+        room = event['room']
+
+        await self.send(text_data=json.dumps({
+                'message':message,
+                'username':username,
+                'room':room,
+
+        }))
+
